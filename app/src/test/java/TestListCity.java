34bb0f0a7aa6429b5e2fc7cab0833962046900c3
Mengju Liao <jky769038@gmail.com>
2021-11-01 13:58:57 -0600

    implement add test

--- app/src/test/java/TestListCity.java
+++ app/src/test/java/TestListCity.java
@@ -12,13 +12,13 @@ public class TestListCity {
 
     private CustomList list;
 
-    @BeforeAll
     public void createList(){
         list = new CustomList(null, new ArrayList<City>());
     }
 
     @Test
     public void addCityTest(){
+        createList();
         int listSize = list.getCount();
         list.addCity(new City("Halifax", "NS"));
         assertEquals(list.getCount(), listSize + 1);
